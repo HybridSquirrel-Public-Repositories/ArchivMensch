003b9708a52d5227bf2f0d6195b1b5d871f83ce4
TheTinybag <lukas@trossvik.com>
2021-02-05 13:46:35 +0100

    Test Commit

--- ArchivMensch/ArchivMensch/Form1.cs
+++ ArchivMensch/ArchivMensch/Form1.cs
@@ -16,5 +16,10 @@ namespace ArchivMensch
         {
             InitializeComponent();
         }
+
+        private void btnClickThis_Click(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "Hello World!";
+        }
     }
 }
